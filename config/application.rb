c59cd043743362ccec021186b22faf9438757d88
johnDv <johndominicvillalino@outlook.com>
2022-10-23 22:13:16 +0800

    enable firebase origin

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module TechDeals
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'https://tech-deals-c9f2f.web.app/'
         resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
       end
     end
