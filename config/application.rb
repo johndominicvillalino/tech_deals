00f4509abe03f32434ffcf78666945e2e3b28c23
johnDv <johndominicvillalino@outlook.com>
2022-10-23 22:23:24 +0800

    cors fix

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module TechDeals
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://tech-deals-c9f2f.web.app/'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
       end
     end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://tech-deals-c9f2f.web.app/'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
     end
   end
